be292eb9003e76075283fb57505d6ee5ef6f9172
PenetRaptor <skyjammer159@gmail.com>
2024-06-10 15:19:43 +0700

    Second task completed

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.geoquiz
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +14,25 @@ class MainActivity : AppCompatActivity() {
 
     private  lateinit var trueButton: Button
     private  lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans,
+            true),
+        Question(R.string.question_mideast,
+            false),
+        Question(R.string.question_africa,
+            false),
+        Question(R.string.question_americas
+            , true),
+        Question(R.string.question_asia,
+            true))
+    private var currentIndex = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -25,22 +45,42 @@ class MainActivity : AppCompatActivity() {
 
         trueButton = findViewById(R.id.button_true)
         trueButton.setOnClickListener{view: View ->
-            Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT
-            ).show()
+            checkAnswer(true)
         }
 
         falseButton = findViewById(R.id.button_false)
         falseButton.setOnClickListener{view: View ->
-            Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT
-            ).show()
+            checkAnswer(false)
         }
 
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.textViewQuestion)
+
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            val questionTextResId = questionBank[currentIndex].textResId
+            questionTextView.setText(questionTextResId)
+        }
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
     }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer:Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId,
+            Toast.LENGTH_SHORT)
+            .show()
+    }
+
 }
 
